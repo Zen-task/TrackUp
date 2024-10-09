7cddbc90da91515fe3b4f2c7a162138b54aa3af4
Zen-task <gkhgkingus@gmail.com>
2024-10-09 22:08:12 +0900

    vue.jsの連携

---
+++ app/javascript/components/ExampleComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="centered-container">
+    <button @click="toggleShow" class="todo_input">button</button>
+    <transition name="bounce">
+      <p v-if="show">Vue.jsでリッチなインタラクション!</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggleShow() {
+      this.show = !this.show;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.centered-container {
+  display: flex;
+  justify-content: center;  /* 水平方向の中央揃え */
+  align-items: center;      /* 垂直方向の中央揃え */
+  min-height: 100vh;        /* ビューポート全体の高さに設定 */
+}
+
+</style>
